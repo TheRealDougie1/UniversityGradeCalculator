2f7383c5449f7f95b0dfa61ffd9b103a16c80cba
Rhys <DouglasRM@cardiff.ac.uk>
2020-12-08 22:25:06 +0000

    [BF-0] Add Placement Year into the calculation

--- GradeCalculator/GradeCalculator.API/UniversityGradeCalculator.cs
+++ GradeCalculator/GradeCalculator.API/UniversityGradeCalculator.cs
@@ -9,10 +9,18 @@
         /// <summary>
         /// Calculates the Final University Grade.
         /// </summary>
-        /// <returns></returns>
-        public double CalculateFinalGrade(int secondYearGrade, int finalYearGrade)
+        /// <param name="secondYearGrade">Overall grade achieved in second year</param>
+        /// <param name="finalYearGrade">Overall grade achieved in final year</param>
+        /// <param name="placementYearGrade">Overall grade achieved in placement year, if taken.</param>
+        /// <returns>The overall final grade achieved</returns>
+        public double CalculateFinalGrade(int secondYearGrade, int finalYearGrade, int placementYearGrade = -1)
         {
-            return secondYearGrade * 0.3 + finalYearGrade * 0.7;
+            if (placementYearGrade == -1)
+            {
+                return secondYearGrade * 0.3 + finalYearGrade * 0.7;
+            }
+
+            return secondYearGrade * 0.2 + placementYearGrade * 0.1 + finalYearGrade * 0.7;
         }
     }
 }
--- GradeCalculator/GradeCalculator.Api.UnitTests/UniversityGradeCalculatorTests.cs
+++ GradeCalculator/GradeCalculator.Api.UnitTests/UniversityGradeCalculatorTests.cs
@@ -25,5 +25,17 @@ namespace GradeCalculator.Api.UnitTests
         {
             sut.CalculateFinalGrade(secondYear, finalYear).Should().Be(result);
         }
+
+        [TestCase(70,70,70,70)]
+        [TestCase(71, 86, 70, 71.8)]
+        [TestCase(100, 100, 100, 100)]
+        [TestCase(0, 0, 0, 0)]
+        [TestCase(70, 0, 0, 14.0)]
+        [TestCase(0, 70, 0, 7)]
+        [TestCase(0, 0, 70, 49.0)]
+        public void CalculateGradeUsingSecondPlacementAndFinalYear_CorrectGradeReturned(int secondYear, int placementYear, int finalYear, double result)
+        {
+            sut.CalculateFinalGrade(secondYear, finalYear, placementYear).Should().Be(result);
+        }
     }
 }
