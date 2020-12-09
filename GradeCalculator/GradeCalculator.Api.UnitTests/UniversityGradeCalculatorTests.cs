02159b6e87bf0c673412740cbe68678c634eddc1
Rhys <DouglasRM@cardiff.ac.uk>
2020-12-09 16:31:31 +0000

    [BF-1] Integrate StyleCop. Add separate Settings.StyleCop files for the Api and UnitTests projects.

--- GradeCalculator/GradeCalculator.Api.UnitTests/UniversityGradeCalculatorTests.cs
+++ GradeCalculator/GradeCalculator.Api.UnitTests/UniversityGradeCalculatorTests.cs
@@ -1,8 +1,9 @@
 namespace GradeCalculator.Api.UnitTests
 {
-    using NUnit.Framework;
-    using GradeCalculator.Api;
+    using System.Diagnostics.CodeAnalysis;
     using FluentAssertions;
+    using GradeCalculator.Api;
+    using NUnit.Framework;
 
     /// <summary>
     /// Unit Tests for the <see cref="UniversityGradeCalculator"/> class
@@ -17,25 +18,25 @@ namespace GradeCalculator.Api.UnitTests
             sut = new UniversityGradeCalculator();
         }
 
-        [TestCase(70,70,70)]
+        [TestCase(70, 70, 70)]
         [TestCase(30, 95, 75.5)]
         [TestCase(95, 30, 49.5)]
         [TestCase(50, 60, 57)]
-        [TestCase(70.2,80.3,77.27)]
+        [TestCase(70.2, 80.3, 77.27)]
         [TestCase(99.9, 99.3, 99.48)]
         public void CalculateGradeUsingSecondAndFinalYear_CorrectGradeReturned(double secondYear, double finalYear, double result)
         {
             sut.CalculateFinalGrade(secondYear, finalYear).Should().Be(result);
         }
 
-        [TestCase(70,70,70,70)]
+        [TestCase(70, 70, 70, 70)]
         [TestCase(71, 86, 70, 71.8)]
         [TestCase(100, 100, 100, 100)]
         [TestCase(0, 0, 0, 0)]
         [TestCase(70, 0, 0, 14.0)]
         [TestCase(0, 70, 0, 7)]
         [TestCase(0, 0, 70, 49.0)]
-        [TestCase(45.8,69.1,50.0,51.07)]
+        [TestCase(45.8, 69.1, 50.0, 51.07)]
         public void CalculateGradeUsingSecondPlacementAndFinalYear_CorrectGradeReturned(double secondYear, double placementYear, double finalYear, double result)
         {
             sut.CalculateFinalGrade(secondYear, finalYear, placementYear).Should().Be(result);
