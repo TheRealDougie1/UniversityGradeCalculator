7a97a83d8e15470487fe3f4339b2364a1f4a8fe0
Rhys <DouglasRM@cardiff.ac.uk>
2020-12-09 21:47:01 +0000

    [BF-2] Partially implement UniversityDegree.

---
+++ GradeCalculator/GradeCalculator.API/IUniversityDegree.cs
@@ -0,0 +1,21 @@
+﻿namespace GradeCalculator.Api
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Interface for University Degree Classes.
+    /// </summary>
+    public interface IUniversityDegree
+    {
+        /// <summary>
+        /// Gets a list of <see cref="IUniversityYear"/>s
+        /// </summary>
+        List<IUniversityYear> ListOfYears { get; }
+
+        /// <summary>
+        /// Add a <see cref="IUniversityYear"/>  to <see cref="ListOfYears"/>
+        /// </summary>
+        /// <param name="universityYear"> University Year to Add </param>
+        void AddYear(IUniversityYear universityYear);
+    }
+}
