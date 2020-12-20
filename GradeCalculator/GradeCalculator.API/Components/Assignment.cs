4b8b92b55b0169b2a6584783865c368bbb4e67f5
Rhys <DouglasRM@cardiff.ac.uk>
2020-12-20 19:37:24 +0000

    [BF-4] Add Assignment.

---
+++ GradeCalculator/GradeCalculator.API/Components/Assignment.cs
@@ -0,0 +1,58 @@
+﻿namespace GradeCalculator.Api.Components
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using GradeCalculator.Api.Interfaces;
+
+    /// <summary>
+    /// Assignment class, used to represent an assignment within a <see cref="Module"/>
+    /// </summary>
+    public class Assignment : IAssignment
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Assignment"/> class.
+        /// </summary>
+        /// <param name="assignmentName"> Name of the assignment </param>
+        /// <param name="weighting"> Weighting of the module </param>
+        /// <param name="overallMark"> Percentage earned on the assignment. </param>
+        public Assignment(string assignmentName, int weighting, double? overallMark = null)
+        {
+            Regex nameRX = new Regex(@"[A-Za-z]");
+
+            if (!nameRX.IsMatch(assignmentName))
+            {
+                throw new ArgumentException("Assignment Name does not satisfy Regex.");
+            }
+
+            if (weighting < 0 || weighting > 100)
+            {
+                throw new ArgumentException("Invalid Assignment weighting.");
+            }
+
+            AssignmentName = assignmentName;
+            Weighting = weighting;
+
+            if (overallMark == null)
+            {
+                return;
+            }
+
+            if (overallMark >= 0 && overallMark <= 100)
+            {
+                OverallMark = (double)overallMark;
+                return;
+            }
+
+            throw new ArgumentException();
+        }
+
+        /// <inheritdoc/>
+        public string AssignmentName { get; private set; }
+
+        /// <inheritdoc/>
+        public int Weighting { get; private set; }
+
+        /// <inheritdoc/>
+        public double OverallMark { get; private set; }
+    }
+}
